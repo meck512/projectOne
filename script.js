3e4cd067f916061c2b33d049455278f4709c1fab
rtuttle93 <82906399+rtuttle93@users.noreply.github.com>
2021-07-07 15:57:02 -0600

    Add files via upload
    Added test script files

---
+++ script.js
@@ -0,0 +1,43 @@
+var userInput = $("#userInput");
+var feedHList = $("#feedList");
+
+
+function artistCards() {
+    fetch("https://app.ticketmaster.com/discovery/v2/events?apikey=7elxdku9GGG5k8j0Xm8KWdANDgecHMV0&keyword=Luke%20Combs&locale=*&size=5")
+        .then(function (response) {
+            return response.json()
+        }).then(function (data) {
+            console.log(data)
+            for (i = 0; i < data._embedded.events.length; i++) {
+                var concert = data._embedded.events[i]
+                console.log(concert)
+                // var ticketPrice = concert.priceRanges[0].min
+                var artistImg = concert.images[0].url
+                var date = concert.dates.start.localDate
+                var venue = concert._embedded.venues[0].name
+                var city = concert._embedded.venues[0].city.name
+
+                // ADD REST INFO
+
+                console.log(venue, city, artistImg, date)
+                var card = document.createElement("div")
+                card.innerHTML = `<div class="card-content white-text">
+                <span class="card-title">Concerts Coming Up</span>
+                <p>Date:</p>
+                <p>Show Time:</p>
+                <p>Venue:</p>
+                <p>City:</p>
+                <p>Hotel:</p>
+                </div>
+                <div class="card-action">
+                <a href="#">Get Tickets</a>
+                <a href="#">Book Flight</a>
+                </div>`
+
+            }
+
+        });
+};
+artistCards()
+
+
